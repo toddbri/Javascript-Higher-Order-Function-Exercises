e1b776875b4f361d40084d9e24aae0d54748a197
Todd Briley <todd.briley@outlook.com>
2017-03-02 22:44:10 -0500

    clarified version of bonus2.js solution

--- bonus2.js
+++ bonus2.js
@@ -2,15 +2,15 @@ function map(arr,fun){
   return arr.map(fun);
 }
 
-
-map(["b","c","d","g","j"],function(item){console.log("1-Give me a "+ item);});
-
-//----------- Alternate Method ------------------------
-console.log("-".repeat(10));
-
 var whatever = function(item){
   return ("Give me a " + item);
 };
+
+var something = map(["b","c","d","g","j"],whatever);
+console.log(something);
+
+//----------- Alternate Method ------------------------
+console.log("-".repeat(10));
 
  var newArray = mapAlt(["b","c","d","g","j"],whatever);
 console.log(newArray);
