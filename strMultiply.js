9a77845a1d9c5244bfe8e526c220c66a022a4b68
Todd Briley <todd.briley@outlook.com>
2017-03-04 18:22:07 -0500

    updated strMultiply exercise solution

--- strMultiply.js
+++ strMultiply.js
@@ -3,17 +3,18 @@ function strMultiply(str,times){
   var arrLoop = range(0,times);
   var arr_result = arrLoop.reduce(joinIt,"");
 
-  function range(min, max) {
-  var arr = [];
-  for (var i = min; i < max; i++) {
-    arr.push(i);
-  }
-  return arr;
+  function joinIt(accumulator,element){ //the element is not used, it can be left off as javascript is tolerant of missing parameters in function calls
+    return accumulator + str;
   }
 
-  function joinIt(output,element,counter){
-    return output +=str;
-  }
+      function range(min, max) {  //This was given by Toby as a substitue for using a for loop directly
+      var arr = [];
+      for (var i = min; i < max; i++) {
+        arr.push(i);
+      }
+      return arr;
+      }
+
   return arr_result;
 }
 
