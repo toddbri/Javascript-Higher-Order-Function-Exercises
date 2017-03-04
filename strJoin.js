55a79eb1a3d924bbcf65b1d7748f0249d5e30262
Todd Briley <todd.briley@outlook.com>
2017-03-04 18:14:56 -0500

    Updated solution to strJoin problem

--- strJoin.js
+++ strJoin.js
@@ -6,12 +6,26 @@ function strJoin(strs,sep){
     } else {
       output +=sep + element;
     }
-    counter +=1;
     return output;
   }
   return strs.reduce(joinIt,"");
 }
 
+// ------Alternate method --------
 
+function strJoinAlternate(strs,sep){
+
+  function joinIt(accumulator, element){
+    if (accumulator.length===0){
+      return element;
+    } else {
+      return accumulator + sep + element;
+    }
+  }
+
+  return strs.reduce(joinIt,"");
+}
 
 console.log(strJoin(['Hello', 'and', 'goodbye'], ' '));
+
+console.log(strJoinAlternate(['Hello', 'and', 'goodbye'], ' '));
